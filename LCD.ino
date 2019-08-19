52c653754a317a845d47afc79e76d4f47a3d2322
Pajoš <fractvival@gmail.com>
2019-08-19 13:50:16 +0200

    Add files via upload

--- LCD.ino
+++ LCD.ino
@@ -1,29 +1,111 @@
+// VERZE 19.8.2019
+// 
+// sestrojeno pro segmenty 8x8 pixelu !
+//
+// LEDControl
+// Eberhard Fahle
+// 1.0.6
 #include "LedControl.h"
 
 // DATA(DIN), CLOCK(CLK), CS
 // Digital piny bez oznaceni D ! pouze cisla
-// Defaultni hodnoty: 12,11,10
-// Posledni hodnota je pocet segmentu
-// Cislo segmentu je v kodu pod oznacenim "adress/addr"
-LedControl lc=LedControl(12,11,10,4);
+// Defaultni hodnoty: 12(DIN),11(CLK),10(CS)
+// Posledni hodnota je pocet segmentu! pocitano od 1
+//
+LedControl lc = LedControl(12,11,10,4);
 
-unsigned long delaytime=500;
-int j = 3;
+/////////////////////////////////////////
+// DEFINICE (pomocnych) PROMENNYCH
+//
+
+// Pauza mezi testem segmentu
+//
+int delayTest = 150;
+//
+// Pauza mezi ctenim tlacitka
+//
+int delayButton = 50;
+//
+// Pauza fce loop
+//
+int delayLoop = 20;
 
 
+// Hlavni pocitadlo ( materialu, tedy flasek :) )
+//
+int Count = 0;
+
+// Intenzita svitivosti
+//
+int Light = 8;
+
+
+// Pocitaci "tlacitko"
+// Klasicke zapojeni tlacitka s 10k odporem na digital pin 
+//
+int Button = 4;
+//
+// RESET tlacitko
+//
+int resetButton = 3;
+
+
+// Zarovnani vypisu
+// 0=leva, 1=prava    strana
+//
+int Align = 0;
+
+
+///////////////////////////
+// NASTAVENI SEGMENTU
+//
 void setup() 
 {
+  delay(300);
+  Serial.begin(9600);
+  // Do promenne "devices" zjistim pocet segmentu (nastaveno vyse)
+  //
   int devices=lc.getDeviceCount();
+
+  // Abychom mohli pracovat s vice segmenty, je potreba je
+  // nejprve vsechny inicializovat a pri te prilezitosti i
+  // otestovat, zda-li sviti vsechny pixely
+  //
   for( int address = 0; address < devices; address++ ) 
   {
+    // Nad segmentem pozastavime fci pro zapinani sporice
     lc.shutdown(address,false);
-    lc.setIntensity(address,4);
+    // Intenzita svitivosti 0..15
+    lc.setIntensity(address,Light);
+    // Zapne vsechny pixely segmentu ADDRESS
     ShowNumber(address,10);
-    delay(1000);
+    delay(delayTest);
+    // Nyni segment "zhasneme"
     lc.clearDisplay(address);
   }
+
+  pinMode(Button,INPUT_PULLUP);
+  digitalWrite(Button,LOW);
+  
+  ///// PRO AKTIVACI RESET TLACITKA NUTNO TUTO CAST ODKOMENTOVAT ! 
+  /*
+  pinMode(resetButton,INPUT_PULLUP);
+  digitalWrite(resetButton,LOW);
+  */
+
+  
+  Show( 0 );
+  
 }
 
+////////////////////////////////
+// DEFINICE CISEL SEGMENTU
+//
+
+
+// NUMBER_X je pouzit pro test pixelu
+// Dojde tedy k aktivaci vsech pixelu
+//
 char NUMBER_X[] = 
 {B11111111,
 B11111111,
@@ -137,15 +219,23 @@ B00000100,
 B00000100,
 B00000100};
 
+
+
+
+// Primo zobrazi, radek po radku, zvolene cislo
+// z predchozich definic cisel..
+//
 void SetDigit( int addr, char * digit )
 {
-   for ( int i = 0; i < 8; i++ )
-   {
-      lc.setRow(addr,i,(byte)digit[i]);
-   }
+  // plati pro segment 8x8 pixelu
+  for ( int i = 0; i < 8; i++ )
+  {
+    lc.setRow(addr,i,(byte)digit[i]);
+  }
 }
 
-
+// Protrideni pozadavku pro zobrazeni cisla
+//
 void ShowNumber(int addr, int number)
 {
   switch( number )
@@ -200,29 +290,135 @@ void ShowNumber(int addr, int number)
       SetDigit(addr,NUMBER_9);
       break;
     }
+    // ZDE pri splneni "10" dojde k rozsviceni vsech pixelu
     case 10:
     {
       SetDigit(addr,NUMBER_X);
       break;
     }
+    // break switche
     break; 
+  }
+  // fce je "void" tudiz nic nevraci..
+}
+
+
+
+///////////////////////////////
+// ZOBRAZUJE CISLA NA DISPLEJI
+//
+void Show( int count )
+{
+  int devices = lc.getDeviceCount();
+
+  for ( int i = 0; i < devices; i++ )
+  {
+    lc.clearDisplay(i);
+  }
+
+  if ( Align == 0 )
+  {
+    if ( Count < 10 )
+    {
+      ShowNumber( devices-1, Count );
+    }
+    if ( (Count > 9) && (Count < 100) )
+    {
+      String buffNumber = (String)Count;
+      buffNumber.c_str();
+      String buff1Number = (String)buffNumber[0];
+      String buff2Number = (String)buffNumber[1];
+      ShowNumber( devices-1, buff1Number.toInt() );
+      ShowNumber( devices-2, buff2Number.toInt() );
+    }
+    if ( (Count > 99) && (Count < 1000) )
+    {
+      String buffNumber = (String)Count;
+      buffNumber.c_str();
+      String buff1Number = (String)buffNumber[0];
+      String buff2Number = (String)buffNumber[1];
+      String buff3Number = (String)buffNumber[2];
+      ShowNumber( devices-1, buff1Number.toInt() );
+      ShowNumber( devices-2, buff2Number.toInt() );
+      ShowNumber( devices-3, buff3Number.toInt() );
+    }
+    if ( (Count > 999) && (Count < 10000) )
+    {
+      String buffNumber = (String)Count;
+      buffNumber.c_str();
+      String buff1Number = (String)buffNumber[0];
+      String buff2Number = (String)buffNumber[1];
+      String buff3Number = (String)buffNumber[2];
+      String buff4Number = (String)buffNumber[3];
+      ShowNumber( devices-1, buff1Number.toInt() );
+      ShowNumber( devices-2, buff2Number.toInt() );
+      ShowNumber( devices-3, buff3Number.toInt() );
+      ShowNumber( devices-4, buff4Number.toInt() );
+    }
+  }
+  else
+  {
+    if ( Count < 10 )
+    {
+      ShowNumber( 0, Count );
+    }
+    if ( (Count > 9) && (Count < 100) )
+    {
+      String buffNumber = (String)Count;
+      buffNumber.c_str();
+      String buff1Number = (String)buffNumber[0];
+      String buff2Number = (String)buffNumber[1];
+      ShowNumber( 1, buff1Number.toInt() );
+      ShowNumber( 0, buff2Number.toInt() );
+    }
+    if ( (Count > 99) && (Count < 1000) )
+    {
+      String buffNumber = (String)Count;
+      buffNumber.c_str();
+      String buff1Number = (String)buffNumber[0];
+      String buff2Number = (String)buffNumber[1];
+      String buff3Number = (String)buffNumber[2];
+      ShowNumber( 2, buff1Number.toInt() );
+      ShowNumber( 1, buff2Number.toInt() );
+      ShowNumber( 0, buff3Number.toInt() );
+    }
+    if ( (Count > 999) && (Count < 10000) )
+    {
+      String buffNumber = (String)Count;
+      buffNumber.c_str();
+      String buff1Number = (String)buffNumber[0];
+      String buff2Number = (String)buffNumber[1];
+      String buff3Number = (String)buffNumber[2];
+      String buff4Number = (String)buffNumber[3];
+      ShowNumber( 3, buff1Number.toInt() );
+      ShowNumber( 2, buff2Number.toInt() );
+      ShowNumber( 1, buff3Number.toInt() );
+      ShowNumber( 0, buff4Number.toInt() );
+    }
   }
 }
 
 
 
+
 void loop() 
 {
-  
-   for ( int i = 0; i < 10; i++ )
-   {
-     ShowNumber(j,i);
-     delay(delaytime);
-   }
-   
-   j--;
+  if ( digitalRead(Button) == HIGH )
+  {
+    Count++;
+    digitalWrite(Button,LOW);
+    Show( Count );
+    delay(delayButton);
+  }
 
-   if ( j == -1 )
-    j = 3;
+  if ( digitalRead(resetButton) == HIGH )
+  {
+    Count = 0;
+    digitalWrite(resetButton,LOW);
+    Show( Count );
+    delay(delayButton);
+  }
 
+  digitalWrite(Button,LOW);
+  delay(delayLoop);
 }
